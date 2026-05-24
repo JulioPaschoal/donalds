2d2fb86e8edd7aa4f4fe25a004c61845446de7bd
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-05-24 03:17:40 -0300

    chore: update css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 const HomePage = () => {
   return (
     <div>
-      <Button className="bg-red-500">FSW Donalds</Button>
+      <Button>FSW Donalds</Button>
       <h1 className="text-red-500">Hello World</h1>
       <Input />
     </div>
